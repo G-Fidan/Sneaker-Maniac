615e4473a450d4c76bcbe5d7702a3d0d7a7ee3e3
Fidan-android <gum.fidan201@yandex.ru>
2022-09-22 18:53:45 +0300

    first commit

---
+++ app/src/main/java/com/example/sneaker_maniac/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sneaker_maniac
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        supportFragmentManager.beginTransaction()
+    }
+}
