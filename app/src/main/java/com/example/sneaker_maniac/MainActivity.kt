15dd3a028566db06efa0c23a250efb61c63fd743
Fidan-android <gum.fidan201@yandex.ru>
2022-09-22 20:49:19 +0300

    Начало разработки

--- app/src/main/java/com/example/sneaker_maniac/MainActivity.kt
+++ app/src/main/java/com/example/sneaker_maniac/MainActivity.kt
@@ -2,15 +2,47 @@ package com.example.sneaker_maniac
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sneaker_maniac.databinding.ActivityMainBinding
+import com.example.sneaker_maniac.fragments.login.LoginFragment
+import com.example.sneaker_maniac.fragments.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onStart() {
         super.onStart()
-        supportFragmentManager.beginTransaction()
+        if (getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE).getString(
+                "token",
+                ""
+            ) == ""
+        ) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(
+                    binding.mainContainer.id,
+                    LoginFragment(),
+                    LoginFragment::javaClass.name
+                )
+                .addToBackStack(LoginFragment::javaClass.name)
+                .commitAllowingStateLoss()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .add(
+                    binding.mainContainer.id,
+                    MainFragment(),
+                    MainFragment::javaClass.name
+                )
+                .addToBackStack(MainFragment::javaClass.name)
+                .commitAllowingStateLoss()
+        }
     }
 }
